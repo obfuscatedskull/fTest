4b8e1475b55407c552435ec7bb26a13a11feb9ca
maky <rikiki12203@gmail.com>
2025-12-16 22:25:40 -0500

    boop

--- script.js
+++ script.js
@@ -32,5 +32,26 @@ let arr = ["bob", [["bobby"]], ["bobaroon"], "bab"];
 
 console.log(arr.flat(50));
 
+let nums = [2,7,11,15];
+let target = 9;
+let storedNumbers = [];
+for (let i = 0; i < nums.length; i++){
+    if (storedNumbers.includes(i)){
+        continue
+    }
+    for (let j = i+1; j < nums.length; j++){
+        if (storedNumbers.includes(j)){
+            continue;
+
+
+        }
+        if (nums[i] + nums[j] === target){
+            storedNumbers.push(i);
+            storedNumbers.push(j);
+        }
+    }
+    
+}
+
 
 
